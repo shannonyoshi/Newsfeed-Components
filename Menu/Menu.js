6910205db42947d854c7001285ce38c4d280fe03
Shannon <shannon.yoshikawa@gmail.com>
2019-05-15 14:04:22 -0700

    completed MVP. something weird with CSS files

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  
+  menu.classList.toggle('menu--open')
 }
 
+
+const menu = document.querySelector('.menu')
+
+const menuButton = document.querySelector('.menu-button')
+
+menuButton.addEventListener('click', toggleMenu)
+
+
+// Toggle the "menu--open" class on your menu refence. 
 // Start Here: Create a reference to the ".menu" class
-const menu;
 // create a reference to the ".menu-button" class
-const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
